72ca3860e84e2111a60ccc40342dfaaaa6c9a5d3
Vlad <promotion-kmv@yandex.ru>
2021-08-10 20:00:15 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+def test():
+    n = 1
+    lst = []
+    number = 32451341
+    while n-1 < number:
+        n += 2
+        if number % n == 0:
+            lst.append(n)
+    print(lst)
+test()
