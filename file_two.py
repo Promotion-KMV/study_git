8eafd7524fdd87bc066e4351163cd6e8a62985fc
Vlad <promotion-kmv@yandex.ru>
2021-08-10 20:26:22 +0300

    'five'

--- file_two.py
+++ file_two.py
@@ -6,4 +6,5 @@ def decorator(func):
         start = time.time()
         func(*args, **kwargs)
         print(round(time.time() - start, 3), 'This time to finish function')
+        print('no ok')
     return wrap
